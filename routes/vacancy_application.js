de5d7b418ecdf53887132eb7c210ca0cb5be1e5c
Bitlab234 <balabanovvovik@mail.ru>
2024-11-29 15:04:03 +0400

    Отклик на вакансию заноситься в базу

--- routes/vacancy_application.js
+++ routes/vacancy_application.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { createVacancyApplication } = require('../models/applicationModel');
+const { createApplication } = require('../models/applicationModel');
+const { getCandidateByUserId } = require('../models/candidateModel');
 
 router.get('/', async (req, res) => {
     const vacancyId = req.query.vacancy_id;
     const user = req.user;
-    
+
+    const candidate = await getCandidateByUserId(user.user_id);
+    const candidateId = candidate.candidate_id;
+    try{
+        await createApplication({
+            candidate_id: candidateId, vacancy_id: vacancyId, status: "pending" 
+        });
+    }catch (error) {
+        console.error('Error creating vacancy:', error);
+        res.status(500).send('Internal Server Error');
+    }
+
     console.log('Vacancy ID: ', vacancyId);
     res.redirect('/vacancies');
 });
